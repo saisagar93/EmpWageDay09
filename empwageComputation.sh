261c35078831d480c10cb7286543ef2aa1bd64d5
saisagar93 <sscreator225@gmail.com>
2020-12-19 12:39:16 +0530

    Add : code to calculate emp wage

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 echo "Welcome to employee Wage Program"
+WagePerHr=20
+FullDayHrs=8
+
 var=$(($RANDOM%2))
 
 if [ $var -eq 1 ]
 then
-        echo "Employee Is Present"
+        dailywages=$(($WagePerHr*$FullDayHrs))
+	echo "The Daily Wage Of Employee Is  : $dailywages"
+
 else
         echo "Employee Is Absent"
 fi
-
