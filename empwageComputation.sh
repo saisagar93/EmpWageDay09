b491597a0a9eeebd3b6d58ef6f5c2838d3e05350
saisagar93 <sscreator225@gmail.com>
2020-12-19 13:16:08 +0530

    code to find emp work hour using function

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -11,12 +11,9 @@ maxHrsInmonth=100
 TotalempHr=0
 TotalWorkingDay=0
 
-while [[ $TotalempHr -le $maxHrsInmonth && $TotalWorkingDay -le $WorkingDays ]]
-do
-        (($TotalWorkingDay+1))
-       empcheck=$(($RANDOM%3))
-
-	case $empcheck in
+function getWorkingHrs ()
+{
+        case $1 in
                         $isFullTime)
                                         empHrs=8
                                 ;;
@@ -29,9 +26,18 @@ do
                                 ;;
 
                 esac
-        salary=$(($empHrs*$EmpRateperHr))
- 		Totalsalary=$(($Totalsalary+$salary))
+
+       echo $empHrs
+}
+
+while [[ $TotalempHrs -le $maxHrsInmonth && $TotalWorkingDays -le $WorkingDays ]]
+do
+        (($TotalWorkingDays+1))
+	empHrs=$( getWorkingHrs $(($RANDOM%3)) )
+	TotalempHrs=$(($TotalempHrs+$empHrs))
 done
 
+totalsalary=$(($TotalempHrs*$EmpRateperHr))
 
-echo "Total Salary For Month Of Employee Is : $Totalsalary"
+
+echo "Total Salary Of Employee Is : $totalsalary"
