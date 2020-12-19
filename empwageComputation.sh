cd917b4279840a2e1027e68d1eaa1f31318e7b47
saisagar93 <sscreator225@gmail.com>
2020-12-19 12:31:43 +0530

    code to check emp attendance

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 echo "Welcome to employee Wage Program"
+var=$(($RANDOM%2))
 
+if [ $var -eq 1 ]
+then
+        echo "Employee Is Present"
+else
+        echo "Employee Is Absent"
+fi
 
